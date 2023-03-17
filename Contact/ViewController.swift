15d1d747762f8f832e33b6b627644bf2189b6158
Kazimir Makaranka <m2014ky@gmail.com>
2023-03-17 16:13:37 +0300

    add table

--- Contact/ViewController.swift
+++ Contact/ViewController.swift
@@ -16,4 +16,19 @@ class ViewController: UIViewController {
 
 
 }
-
+  
+extension ViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // получаем экземляр ячейки
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)// инициализатор который возвращает ячейку на основе предустановленного стиля
+        // конфигурируем ячейку
+        var configuration = cell.defaultContentConfiguration()// создаем ячейку без данных
+        configuration.text = "Строка \(indexPath.row)"//наполняем ее данными
+        cell.contentConfiguration = configuration // наполненная конфигурация передается ячейке
+        // возвращаем сконфигурированный экземпляр ячейки
+        return cell
+    }
+}
